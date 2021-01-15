84b7a74749fa942bcdccc3f24db518d9303ebdbe
costel-dev <costelcuconoiu@gmail.com>
2021-01-15 15:50:36 +0100

    firebase setup added

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDxvZDYEiQLw1FYFIKEojK8dXzQxWiY08s',
+  authDomain: 'vue-calendar-a915d.firebaseapp.com',
+  databaseURL: 'https://vue-calendar-a915d.firebaseio.com',
+  projectId: 'vue-calendar-a915d',
+  storageBucket: 'vue-calendar-a915d.appspot.com',
+  messagingSenderId: '1011366733158',
+  appId: '1:1011366733158:web:8757adca9c1cfadd5f54a7',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp };
